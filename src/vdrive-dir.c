dhansel/VDrive

commit a09c68a2485578ae156e31bdbc22c6c1e94dd8c1

For disks with endlessly looped directories: - don't get stuck in endless loop in VDriveClass:getFileNumBlocks() - for "$:name", if name does NOT include a wildcard then stop listing after first match

src/vdrive-dir.c
@@ -259,7 +259,14 @@ static unsigned int date_to_int(int year, int month, int day, int hour, int minu
     return a;
 }
 
+
 uint8_t *vdrive_dir_find_next_slot(vdrive_dir_context_t *dir)
+{
+  return vdrive_dir_find_next_slot_limited(dir, -1);
+}
+
+
+uint8_t *vdrive_dir_find_next_slot_limited(vdrive_dir_context_t *dir, int search_max_slots)
 {
     static uint8_t return_slot[32];
     vdrive_t *vdrive = dir->vdrive;
@@ -312,8 +319,23 @@ uint8_t *vdrive_dir_find_next_slot(vdrive_dir_context_t *dir)
             /* time_low is initially 0, and time_high is initially largest,
                 so it should always match for most uses. */
             if (t >= dir->time_low && t <= dir->time_high)
+              {
+                /* if there were no wildcards then stop looking for more files
+                   after the first match (important if the directory has an endless loop) */
+                dir->find_nslot[dir->find_length]=0;
+                if( cbmdos_parse_wildcard_check(dir->find_nslot, dir->find_length)==0 )
+                  {
+                    dir->slot = 8;
+                    dir->buffer[0] = 0;
+                  }
+
                 return return_slot;
+              }
         }
+
+        if( search_max_slots>0 && --search_max_slots==0 )
+          return NULL;
+
     } while (1);
 
 #ifdef DEBUG_DRIVE

src/VDriveClass.cpp
@@ -199,7 +199,7 @@ int VDrive::getFileNumBlocks(const char *name, bool convertNameToPETSCII)
   // find first non-DEL file that matches the pattern (same as LOAD would do)
   uint8_t *slot;
   do 
-    { slot = vdrive_dir_find_next_slot(&dir); }
+    { slot = vdrive_dir_find_next_slot_limited(&dir, 144); }
   while( slot && ((slot[SLOT_TYPE_OFFSET] & 0x07) == CBMDOS_FT_DEL) );
 
   return slot ? slot[SLOT_NR_BLOCKS] | (slot[SLOT_NR_BLOCKS + 1] << 8) : -1;

src/vdrive-dir.h
@@ -79,6 +79,7 @@ int vdrive_dir_first_directory(struct vdrive_s *vdrive, struct cbmdos_cmd_parse_
 int vdrive_dir_next_directory(struct vdrive_s *vdrive, struct bufferinfo_s *b);
 void vdrive_dir_find_first_slot(struct vdrive_s *vdrive, const uint8_t *name, int length, unsigned int type, vdrive_dir_context_t *dir);
 uint8_t *vdrive_dir_find_next_slot(vdrive_dir_context_t *dir);
+uint8_t *vdrive_dir_find_next_slot_limited(vdrive_dir_context_t *dir, int search_max_slots);
 void vdrive_dir_no_a0_pads(uint8_t *ptr, int l);
 int vdrive_dir_filetype(const uint8_t *name, int length);
 void vdrive_dir_remove_slot(vdrive_dir_context_t *dir);

commit acd105ba65d602f2afade59cda42ec2664d80667

Fixed "S:" (scratch) command (broke in a09c68a commit)

src/vdrive-dir.c
@@ -320,15 +320,6 @@ uint8_t *vdrive_dir_find_next_slot_limited(vdrive_dir_context_t *dir, int search
                 so it should always match for most uses. */
             if (t >= dir->time_low && t <= dir->time_high)
               {
-                /* if there were no wildcards then stop looking for more files
-                   after the first match (important if the directory has an endless loop) */
-                dir->find_nslot[dir->find_length]=0;
-                if( cbmdos_parse_wildcard_check(dir->find_nslot, dir->find_length)==0 )
-                  {
-                    dir->slot = 8;
-                    dir->buffer[0] = 0;
-                  }
-
                 return return_slot;
               }
         }
@@ -722,10 +713,18 @@ int vdrive_dir_next_directory(vdrive_t *vdrive, bufferinfo_t *b)
 {
     uint8_t *l, *p;
     int blocks, i;
+    int have_wildcard = cbmdos_parse_wildcard_check(b->dir.find_nslot, b->dir.find_length);
 
     b->small = 0;
 
     while ((p = vdrive_dir_find_next_slot(&b->dir))) {
+        /* if there were no wildcards then stop looking for more files
+           after the first match (important if the directory has an endless loop) */
+        if( !have_wildcard ) {
+            b->dir.slot = 8;
+            b->dir.buffer[0] = 0;
+          }
+
         if (p[SLOT_TYPE_OFFSET]) {
             l = b->buffer + b->bufptr;
 